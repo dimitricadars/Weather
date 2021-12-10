176b5a84e5ad74112f724efa0c28f178c579644a
Dimitri CADARS <dimitricadars@gmail.com>
2021-12-10 21:44:24 +0100

    Init Weather Project

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Dimitri CADARS on 10/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
